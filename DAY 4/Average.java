4b45ec189c16a3bee23e6abe741150f88c4374b5
Kalai410 <137813296+Kalai410@users.noreply.github.com>
2024-04-14 21:01:46 +0530

    Add files via upload

---
+++ DAY 4/Average.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Average
+{
+public static void main(String args[])
+{
+int a=24;
+int b=23;
+int c=47;
+int d;
+d=((a+b+c)/3);
+System.out.println("Average of three numbers : "+d);
+}
+}
