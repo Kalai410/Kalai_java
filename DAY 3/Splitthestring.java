876e72e1ddf5a21710df935d171d6200da6df489
Kalai410 <137813296+Kalai410@users.noreply.github.com>
2024-04-14 19:18:27 +0530

    Add files via upload

---
+++ DAY 3/Splitthestring.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Splitthestring
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your fullname :");
+String fname=sc.nextLine();
+String lname=sc.nextLine();
+System.out.println(fname + " " + lname);
+}
+}
