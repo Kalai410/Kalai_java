78548a6a49274e80ac4b997e4e7d5826ede62638
Kalai410 <137813296+Kalai410@users.noreply.github.com>
2024-04-15 18:30:26 +0530

    Average.java

--- DAY 3/Average.java
+++ DAY 3/Average.java
@@ -3,10 +3,10 @@ class Average
 {
 public static void main(String args[])
 {
-int a=24;
+int a=17;
 int b=23;
-int c=47;
-int d;
+int c=15;
+float d;
 d=((a+b+c)/3);
 System.out.println("Average of three numbers : "+d);
 }
