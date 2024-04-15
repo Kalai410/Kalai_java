a7503f9bfdf6c6341900d49079dbe686cf74cd6f
Kalai410 <137813296+Kalai410@users.noreply.github.com>
2024-04-15 18:50:36 +0530

    Splitthestring.java

--- DAY 3/Splitthestring.java
+++ DAY 3/Splitthestring.java
@@ -5,8 +5,9 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter your fullname :");
-String fname=sc.nextLine();
-String lname=sc.nextLine();
-System.out.println(fname + " " + lname);
+String fname=sc.next();
+System.out.println("First name :"+fname);
+String lname=sc.next();
+System.out.println("Last name :"+lname);
 }
 }
